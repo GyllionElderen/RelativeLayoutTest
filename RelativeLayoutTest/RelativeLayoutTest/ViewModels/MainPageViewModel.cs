ed56e65c581355f74dc330b377dfab6b02e4cf30
Gyllion van Elderen <gyllion.van.elderen@appstones.nl>
2018-05-01 10:08:24 +0200

    Add project files.

---
+++ RelativeLayoutTest/RelativeLayoutTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelativeLayoutTest.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public DelegateCommand TestCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+            TestCommand = new DelegateCommand(Test);
+        }
+
+        private void Test()
+        {
+            var test = "This is a test";
+        }
+    }
+}
